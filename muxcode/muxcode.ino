238448a61253a5009c215228b40a782e56f6d9f5
CardboardBread <mike_brown@rogers.com>
2015-12-08 10:44:24 -0500

    pin setup complete

--- muxcode/muxcode.ino
+++ muxcode/muxcode.ino
@@ -1,8 +1,15 @@
+const int muxEnable = 5;
+int muxSignal[3];
+int buttonPins[4];
+
 void setup() {
-  for (int x = 2; x < 5) {
-    int muxSignal[x - 2] = x;  
+  for (int x = 2; x < 5; x++) {
+    muxSignal[x - 2] = x;  
   }
-  pinMode(int muxEnable, OUTPUT);
+  for (int x = 6; x <10; x++) {
+    buttonPins[x - 6] = x;
+  }
+  pinMode(muxEnable, OUTPUT);
 }
 
 void loop() {
