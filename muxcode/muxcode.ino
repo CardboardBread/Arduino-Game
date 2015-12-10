23f3c02484a71d9c4fdeaf3ac7b36779361ae78d
Michael Brown <mike_brown@rogers.com>
2015-12-09 19:42:19 -0500

    Multiplexer Use
    alternate take on using the multiplexer to control LEDs

--- muxcode/muxcode.ino
+++ muxcode/muxcode.ino
@@ -1,6 +1,9 @@
-const int muxEnable = 5;
-int muxSignal[3];
+const int muxEnable1 = 5;
+const int muxEnable2;
+int muxSignal1[3];
+int muxSignal2[3];
 int buttonPins[4];
+int randChoose = 0;
 
 void setup() {
   for (int x = 2; x < 5; x++) {
@@ -12,6 +15,60 @@ void setup() {
   pinMode(muxEnable, OUTPUT);
 }
 
-void loop() {
+void writeToMultiplexer (int ledPos) {
+  int bit1, bit2, bit3, mux = 0;
   
+  bit3 = ledPos % 2;
+  ledPos = ledPos / 2;
+  bit2 = ledPos % 2;
+  ledPos = ledPos / 2;
+  bit1 = ledPos % 2;
+    
+  if (ledPos > -1 && ledPos < 4) {
+    mux = 1;
+  } else if (ledPos > 7 && ledPos < 12) {
+    mux = 1;
+  } else if (ledPos > 3 && ledPos < 8) {
+    mux = 2;
+  } else if (ledPos > 11 && ledPos < 16) {
+    mux = 2;
+  }
+  
+  if (mux == 1) {
+    if (bit1 == 0) {
+      digitalWrite(muxSignal1[1], LOW);
+    } else if (bit1 == 1) {
+      digitalWrite(muxSignal1[1], HIGH);
+    }
+    if (bit2 == 0) {
+      digitalWrite(muxSignal1[2], LOW);
+    } else if (bit2 == 1) {
+      digitalWrite(muxSignal1[2], HIGH);
+    }
+    if (bit3 == 0) {
+      digitalWrite(muxSignal1[3], LOW);
+    } else if (bit3 == 1) {
+      digitalWrite(muxSignal1[3], HIGH);
+    }
+  } else if (mux == 2) {
+    if (bit1 == 0) {
+      digitalWrite(muxSignal2[1], LOW);
+    } else if (bit1 == 1) {
+      digitalWrite(muxSignal2[1], HIGH);
+    }
+    if (bit2 == 0) {
+      digitalWrite(muxSignal2[2], LOW);
+    } else if (bit2 == 1) {
+      digitalWrite(muxSignal2[2], HIGH);
+    }
+    if (bit3 == 0) {
+      digitalWrite(muxSignal2[3], LOW);
+    } else if (bit3 == 1) {
+      digitalWrite(muxSignal2[3], HIGH);
+    }
+  }
+}
+
+void loop() {
+  writeToMultiplexer(2);
 }
