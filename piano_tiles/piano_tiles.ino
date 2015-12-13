edda1969686bab836bba8ca3ca9ef9a02fb927ab
Vithuran Sadagopan <vithuransoccer1@hotmail.com>
2015-12-13 18:02:20 -0500

    the function in this program works but only for one multiplexar

--- piano_tiles/piano_tiles.ino
+++ piano_tiles/piano_tiles.ino
@@ -1,88 +1,67 @@
-const int channelA1 = 3;
-const int channelB1 = 4;
-const int channelC1 = 5;
+const int channelA1 = 2;
+const int channelB1 = 3;
+const int channelC1 = 4;
+const int enable1   = 5;
+
 const int channelA2 = 6;
 const int channelB2 = 7;
 const int channelC2 = 8;
-const int button1   = 9;
-const int button2   = 10;
-const int button3   = 11;
-const int button4   = 12;
-long randnum;
+const int enable2   = 9;
+
+const int button1   = 10;
+const int button2   = 11;
+const int button3   = 12;
+const int button4   = 13;
 int Led;
 
+
+
 void setup() {
+  // put your setup code here, to run once:
   Serial.begin(9600);
-  for (int x = 3; x < 9; x++) {
-    pinMode(x, OUTPUT);
+  pinMode(2, OUTPUT);
+  pinMode(3, OUTPUT);
+  pinMode(4, OUTPUT);
+  pinMode(5, OUTPUT);
+}
+void loop() {
+  for(int Led = 0; Led < 8; Led++){
+  delay(500);
+  binarysignal(Led);
   }
-  for (int x = 9; x < 13; x++) {
-    pinMode(x, INPUT);
-  }
-  randomSeed(analogRead(0));
 }
 
 void binarysignal (int lednum) {
-  int A, B, C, x = 0 ;
-  if (lednum > 7) {
-    lednum = lednum - 8;
-    x++;
-  }
-  C = lednum % 2;
+  int A, B, C = 0 ;
+
+  A = lednum % 2;
   lednum = lednum / 2;
   B = lednum % 2;
   lednum = lednum / 2;
-  A = lednum % 2;
+  C = lednum % 2;
 
-  if (x = 0) {
-    if (A == 0) {
-      digitalWrite(channelA1, LOW);
-    } else {
-      digitalWrite(channelA1, HIGH);
-    }
-    if (B == 0) {
-      digitalWrite(channelB1, LOW);
-    } else {
-      digitalWrite(channelB1, HIGH);
-    }
-    if (C == 0) {
-      digitalWrite(channelC1, LOW);
-    } else {
-      digitalWrite(channelC1, HIGH);
-    }
-  } else {
-    if (A == 0) {
-      digitalWrite(channelA2, LOW);
-    } else {
-      digitalWrite(channelA2, HIGH);
-    }
-    if (B == 0) {
-      digitalWrite(channelB2, LOW);
-    } else {
-      digitalWrite(channelB2, HIGH);
-    }
-    if (C == 0) {
-      digitalWrite(channelC2, LOW);
-    } else {
-      digitalWrite(channelC2, HIGH);
-    }
+  digitalWrite (enable1, LOW);
+  if (A == 0) {
+    digitalWrite(channelA1, LOW);
   }
+  else {
+    digitalWrite(channelA1, HIGH);
+  }
+  if (B == 0) {
+    digitalWrite(channelB1, LOW);
+  }
+  else {
+    digitalWrite(channelB1, HIGH);
+  }
+  if (C == 0) {
+    digitalWrite(channelC1, LOW);
+  }
+  else {
+    digitalWrite(channelC1, HIGH);
+  }
+
+  Serial.print(A);
+  Serial.print(B);
+  Serial.print(C) ;
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
-  randnum = random(0, 3);
-  Serial.println(randnum);
-
-  if (randnum = 0) {
-    Led = 0;
-  } else if (randnum = 1) {
-    Led = 4;
-  } else if (randnum = 2) {
-    Led = 8;
-  } else {
-    Led = 12;
-  }
-  binarysignal(2);
-  digitalWrite(5, HIGH);
-}
