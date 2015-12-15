9c2401768409477b69ba76e293e211fe11232a2b
Vithop <vithuransoccer1@hotmail.com>
2015-12-15 11:11:09 -0500

    Update piano_tiles.ino
    can turn on 4 leds now

--- piano_tiles/piano_tiles.ino
+++ piano_tiles/piano_tiles.ino
@@ -1,88 +1,97 @@
-const int channelA1 = 3;
-const int channelB1 = 4;
-const int channelC1 = 5;
+const int channelA1 = 2;
+const int channelB1 = 3;
+const int channelC1 = 4;
+const int enable1   = 5;
+
 const int channelA2 = 6;
 const int channelB2 = 7;
 const int channelC2 = 8;
-const int button1   = 9;
-const int button2   = 10;
-const int button3   = 11;
-const int button4   = 12;
-long randnum;
-int Led;
+const int enable2   = 9;
+
+const int button1   = 10;
+const int button2   = 11;
+const int button3   = 12;
+const int button4   = 13;
+int Led = 0;
+
+
 
 void setup() {
+  // put your setup code here, to run once:
   Serial.begin(9600);
-  for (int x = 3; x < 9; x++) {
-    pinMode(x, OUTPUT);
+  for (int x = 0; x < 10; x++){
+    pinMode(x,OUTPUT);
   }
-  for (int x = 9; x < 13; x++) {
-    pinMode(x, INPUT);
+}
+void loop() {
+  for(int Led = 0; Led < 4; Led++){
+    for (int time = 0; time < 50; time++){
+      delay(5);
+      binarysignal2(Led);
+      binarysignal1(Led+1);
+      delay(5);
+      binarysignal2(Led + 4+2);
+      binarysignal1(Led + 4+3);
+    }
   }
-  randomSeed(analogRead(0));
 }
 
-void binarysignal (int lednum) {
-  int A, B, C, x = 0 ;
-  if (lednum > 7) {
-    lednum = lednum - 8;
-    x++;
-  }
-  C = lednum % 2;
+void binarysignal1 (int lednum) {
+  int A, B, C;
+
+  A = lednum % 2;
   lednum = lednum / 2;
   B = lednum % 2;
   lednum = lednum / 2;
-  A = lednum % 2;
+  C = lednum % 2;
 
-  if (x = 0) {
-    if (A == 0) {
-      digitalWrite(channelA1, LOW);
-    } else {
-      digitalWrite(channelA1, HIGH);
-    }
-    if (B == 0) {
-      digitalWrite(channelB1, LOW);
-    } else {
-      digitalWrite(channelB1, HIGH);
-    }
-    if (C == 0) {
-      digitalWrite(channelC1, LOW);
-    } else {
-      digitalWrite(channelC1, HIGH);
-    }
-  } else {
-    if (A == 0) {
-      digitalWrite(channelA2, LOW);
-    } else {
-      digitalWrite(channelA2, HIGH);
-    }
-    if (B == 0) {
-      digitalWrite(channelB2, LOW);
-    } else {
-      digitalWrite(channelB2, HIGH);
-    }
-    if (C == 0) {
-      digitalWrite(channelC2, LOW);
-    } else {
-      digitalWrite(channelC2, HIGH);
-    }
+  digitalWrite (enable2, LOW);
+  if (A == 0) {
+    digitalWrite(channelA2, LOW);
   }
+  else {
+    digitalWrite(channelA2, HIGH);
+  }
+  if (B == 0) {
+    digitalWrite(channelB2, LOW);
+  }
+  else {
+    digitalWrite(channelB2, HIGH);
+  }
+  if (C == 0) {
+    digitalWrite(channelC2, LOW);
+  }
+  else {
+    digitalWrite(channelC2, HIGH);
+  } 
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
-  randnum = random(0, 3);
-  Serial.println(randnum);
+void binarysignal2 (int lednum) {
+  int A, B, C;
 
-  if (randnum = 0) {
-    Led = 0;
-  } else if (randnum = 1) {
-    Led = 4;
-  } else if (randnum = 2) {
-    Led = 8;
-  } else {
-    Led = 12;
+  A = lednum % 2;
+  lednum = lednum / 2;
+  B = lednum % 2;
+  lednum = lednum / 2;
+  C = lednum % 2;
+
+  digitalWrite (enable1, LOW);
+  if (A == 0) {
+    digitalWrite(channelA1, LOW);
   }
-  binarysignal(2);
-  digitalWrite(5, HIGH);
+  else {
+    digitalWrite(channelA1, HIGH);
+  }
+  if (B == 0) {
+    digitalWrite(channelB1, LOW);
+  }
+  else {
+    digitalWrite(channelB1, HIGH);
+  }
+  if (C == 0) {
+    digitalWrite(channelC1, LOW);
+  }
+  else {
+    digitalWrite(channelC1, HIGH);
+  } 
 }
