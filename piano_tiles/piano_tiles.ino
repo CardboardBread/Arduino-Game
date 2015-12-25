01b7a12416fcc58e8993c22664a037a42fdb0279
Vithop <vithuransoccer1@hotmail.com>
2015-12-24 21:42:11 -0500

    IT WORKS!!!!!!!!!!
    an actual playable game with no scoring or sound effects 
    will move in accordance to the buttons pressed

--- piano_tiles/piano_tiles.ino
+++ piano_tiles/piano_tiles.ino
@@ -1,3 +1,4 @@
+//the goody two shoe program
 const int channelA1 = 2;
 const int channelB1 = 3;
 const int channelC1 = 4;
@@ -8,11 +9,12 @@ const int channelB2 = 7;
 const int channelC2 = 8;
 const int enable2   = 9;
 
-const int button1   = 10;
-const int button2   = 11;
-const int button3   = 12;
-const int button4   = 13;
+const int pushbutton1   = A1;
+const int pushbutton2   = A2;
+const int pushbutton3   = 12;
+const int pushbutton4   = 13;
 int Led = 0;
+int tilecolumn[4] = {2, 3, 4, 1};
 
 
 
@@ -22,63 +24,118 @@ void setup() {
   for (int x = 0; x < 10; x++) {
     pinMode(x, OUTPUT);
   }
+  pinMode(pushbutton1, INPUT);
+  pinMode(pushbutton2, INPUT);
+  pinMode(pushbutton3, INPUT);
+  pinMode(pushbutton4, INPUT);
+
+  randomSeed(analogRead(A5));
+
 }
 void loop() {
-  /*for (int Led = 0; Led < 4; Led++) {
-    for (int time = 0; time < 100; time++) {
-      delay(1);
-      LED_ON(3, Led);
-        delay(1);
-      LED_ON(4, Led);
-        delay(1);
-      LED_ON(3, Led + 2);
-        delay(1);
-      LED_ON(4, Led + 2);
-      }
-    }*/
-   tiledroper(2,1,4,4); 
-  }
-void tiledroper(int coltile1,int coltile2,int coltile3,int coltile4){
-    for (int time = 0; time < 100; time++) {
-      delay(1);
-      LED_ON(coltile1, 0);
-        delay(1);
-      LED_ON(coltile2, 1);
-        delay(1);
-      LED_ON(coltile3, 2);
-        delay(1);
-      LED_ON(coltile4, 3);
-      }
+
+
+  tiler(tilecolumn[0], tilecolumn[1], tilecolumn[2], tilecolumn[3]);
+  shifter(tilecolumn);
 }
 
-  
-void LED_ON(int col, int row) {
-    //for (int Led = 0; Led < 4; Led++) {
-    //delay(1000);
-    if (row > 3){
-      row = 8;
-    }
-    else {
-    row = row;
-    }
-    if (col == 1) {
-      binarysignal1(row);
-    }
-    else if (col == 2) {
-      binarysignal2(row);
+void shifter(int tiles[4]) {
+  tiles[3] = tiles[2];
+  delay(1);
+  tiles[2] = tiles[1];
+  delay(1);
+  tiles[1] = tiles[0];
+  delay(1);
+  tiles[0] = random(1, 5);
+  delay(1);
 
-    }
-    else if (col == 3) {
-      binarysignal1(row + 4);
+}
 
-    }
-    else {
-      binarysignal2(row + 4);
-
-    }
-
-    //}
+void tiler(int coltile1, int coltile2, int coltile3, int coltile4) {
+  //for (int time = 0; time < 100; time++) {
+  int x = 0;
+  while (x < 1) {
+    delay(1);
+    digitalWrite (enable1, HIGH);
+    digitalWrite (enable2, HIGH);
+    LED_ON(coltile1, 0);
+    delay(1);
+    LED_ON(coltile2, 1);
+    delay(1);
+    LED_ON(coltile3, 2);
+    delay(1);
+    LED_ON(coltile4, 3);
+    x = Buttons(coltile4);
   }
+}
+
+int Buttons (int correctled) {
+  int tap;
+  if (correctled == 4) {
+    tap = pushbutton4;
+  }
+  else if (correctled == 3) {
+    tap = pushbutton3;
+  }
+  else if (correctled == 2) {
+    tap = pushbutton2;
+  }
+  else {
+    tap = pushbutton1;
+  }
+
+  int x = 0;
+  //while ( x < 1) {
+  delay(1);
+
+  int buttonState = digitalRead(tap);
+
+  if (buttonState == 0) {
+    Serial.println(buttonState);
+    delay(10);
+    int buttonState = digitalRead(tap);
+    if (buttonState == 1) {
+      Serial.println("tapped");
+      Serial.println(correctled);
+      x = 1;
+    }
+    else {
+      x = 0;
+    }
+  }
+  else {
+    x = 0;
+  }
+  //}
+  return x;
+}
+
+void LED_ON(int col, int row) {
+
+  if (row > 3) {
+    row = 8;
+  }
+  else {
+    row = row;
+  }
+  if (col == 1) {
+    binarysignal1(row);
+  }
+  else if (col == 2) {
+    binarysignal2(row);
+
+  }
+  else if (col == 3) {
+    binarysignal1(row + 4);
+
+  }
+  else {
+    binarysignal2(row + 4);
+
+  }
+}
+
+
 void binarysignal2 (int lednum) {
   int A, B, C;
   if (lednum >= 8) {
@@ -145,5 +202,5 @@ void binarysignal1 (int lednum) {
   else {
     digitalWrite(channelC1, HIGH);
   }
-}
 
+}
